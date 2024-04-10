684b848b79e28f56bd22ff112f5430431eb9c2fb
ValerianFourel <77210325+ValerianFourel@users.noreply.github.com>
2024-04-10 14:48:44 +0900

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -9,18 +9,22 @@ bash fetch_data.sh
 bash fetch_bodymodels.sh
 bash install_thirdparty.sh
 
+gdown https://github.com/Arthur151/ROMP/releases/download/V2.0/smpl_model_data.zip
+unzip smpl_model_data.zip
+
 
 cp essentials/body_models/smpl/SMPL_NEUTRAL.pkl smpl_model_data/SMPL_NEUTRAL.pkl
-cp smpl/SMPL_NEUTRAL.pkl smpl_model_data/SMPL_NEUTRAL.pkl
 pip install numpy==1.22.4
 romp.prepare_smpl -source_dir=smpl_model_data
 
 cp -r  essentials/body_models/smil smpl_model_data/smil
-bev.prepare_smil -source_dir=buddi/essentials/body_models
 
-cp -r .romp buddi/essentials/body_models/smpla
 cp smpl_model_data/J_regressor_extra.npy essentials/body_models/J_regressor_extra.npy
+
+cd ..
 cp buddi/smpl_model_data/J_regressor_h36m.npy buddi/essentials/body_models/J_regressor_h36m.npy
+bev.prepare_smil -source_dir=buddi/essentials/body_models
+cp .romp/SMPLA_NEUTRAL.pth buddi/essentials/body_models/smpla/SMPLA_NEUTRAL.pth
 cp ../.romp/smil_packed_info.pth essentials/body_models/smil/smil_packed_info.pth
 
 bash demo.sh
