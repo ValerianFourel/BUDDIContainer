62927ff27edcd1171817bd810fd3e76beb687b9c
ValerianFourel <77210325+ValerianFourel@users.noreply.github.com>
2024-04-27 16:11:55 +0900

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -4,7 +4,9 @@ bash install_conda_envContainer.sh
 eval "$(conda shell.bash hook)"
 conda activate hhcenv39
 
-cd ../buddi
+cd ..
+git clone https://github.com/muelea/buddi.git
+cd buddi
 bash fetch_data.sh
 bash fetch_bodymodels.sh
 bash install_thirdparty.sh
